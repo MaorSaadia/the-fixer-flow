4eb74b5ba067359d85d0ec902b1a06ff51c52aab
Maor <maorsa9@gmail.com>
2025-10-29 11:50:41 +0200

    added new post

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Base URL - Update this to your production URL
 const baseUrl = "https://thefixerflow.com";
 
 export const metadata: Metadata = {
