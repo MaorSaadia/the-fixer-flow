68431a5fd9f31fe3204be8bfa315100859c9ad8e
Maor <maorsa9@gmail.com>
2025-10-31 15:05:42 +0200

    delete the desription from product sanity

--- sanity/schemaTypes/product.ts
+++ sanity/schemaTypes/product.ts
@@ -1,36 +1,31 @@
 export default {
-  name: 'product',
-  title: 'Product',
-  type: 'document',
+  name: "product",
+  title: "Product",
+  type: "document",
   fields: [
     {
-      name: 'productName',
-      title: 'Product Name',
-      type: 'string',
+      name: "productName",
+      title: "Product Name",
+      type: "string",
     },
     {
-      name: 'productImage',
-      title: 'Product Image',
-      type: 'image',
+      name: "productImage",
+      title: "Product Image",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
     {
-      name: 'affiliateLink',
-      title: 'Affiliate Link',
-      type: 'url',
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: "affiliateLink",
+      title: "Affiliate Link",
+      type: "url",
     },
   ],
   preview: {
     select: {
-      title: 'productName',
-      media: 'productImage',
+      title: "productName",
+      media: "productImage",
     },
   },
-}
+};
